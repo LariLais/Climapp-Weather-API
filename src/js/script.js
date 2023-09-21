6df01a0f35a62d3d5017ccbf981cc11b0da4ed38
ingrid <inggmyg@gmail.com>
2023-09-21 08:15:04 -0300

    <Estrutura do carrossel pronto>

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,19 @@
 // Variáveis
 const mode = document.querySelector(".toggle")
-
+var cont = 1
+var radio = document.querySelector('.manual-btn')
+document.getElementById('radio1').checked = true
 // Funções
+setInterval(() => {
+    proximaImg()
+},3000)
+
+function proximaImg(){
+    cont++
+    if(cont > 3)
+    cont = 1
+    document.getElementById('radio'+cont).checked = true
+}
 
 function reload() {
     location.reload()
@@ -18,6 +30,8 @@ mode.addEventListener("click", toggleMode)
 // Chamadas de Função
 
 toggleMode()
+
+
 
 
 
